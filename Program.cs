ac850aba6025007a7edae9e036d23a253c9c56c4
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-03-07 23:27:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stena_of_China
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "#";
+            string str10 = str + str + str + str + str + str + str + str + str + str;
+            string str100 = str10 + str10 + str10 + str10 + str10 + str10 + str10 + str10 + str10 + str10;
+            string str1000 = str100 + str100 + str100 + str100 + str100 + str100 + str100 + str100 + str100 + str100;
+            Console.WriteLine(str1000);
+        }
+    }
+}
